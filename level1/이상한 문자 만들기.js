754975c64c6d14b95bd88637eda2c241736e8ddc
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2021-09-10 00:04:59 +0900

    Update : 이상한 문자 만들기.js

--- level1/이상한 문자 만들기.js
+++ level1/이상한 문자 만들기.js
@@ -3,8 +3,9 @@ function solution(s) {
     var isUp = true;
     
     for(let i = 0; i<s.length;i++){
+        
         if(isUp) answer = answer+s[i].toUpperCase();
-        else answer = answer+s[i];
+        else answer = answer+s[i].toLowerCase();
         
         isUp = !isUp;
         if(s[i] === ' ') isUp = true;
