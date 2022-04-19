f24e507616c1f814d23d34b7a1f9ae58aac6a53c
suyeoniii <ssy4230@naver.com>
2022-04-19 23:10:56 +0900

    level1: 예산

---
+++ level1/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+  var answer = 0;
+
+  d.sort((a, b) => a - b);
+  for (let money of d) {
+    budget -= money;
+    if (budget < 0) break;
+
+    answer++;
+  }
+
+  return answer;
+}
